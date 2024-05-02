12801cd70db740ea2389e7e9225e59301413d97d
Praveen Kumar187 <prave187@in.ibm.com>
2024-05-02 12:51:40 +0530

    Upload

--- utilities/customlogger.py
+++ utilities/customlogger.py
@@ -2,7 +2,7 @@ import logging
 class LogGen:
     @staticmethod
     def loggen():
-        logging.basicConfig(filename="C:\\Users\\PraveenKumar187\\Documents\\S2P\\Automation\\Content Intelligence\\Pytest\\Logs\\indexingscreen.log", format='%(asctime)s-%(levelname)s-%(message)s',
+        logging.basicConfig(filename="./Logs/indexingscreen.log", format='%(asctime)s-%(levelname)s-%(message)s',
                             datefmt='%d-%b-%y %H:%M:%S')
         logger = logging.getLogger()
         logger.setLevel(logging.INFO)
