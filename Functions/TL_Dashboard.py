30021852daf3a46d2c523f5c20c0cfd76c4b17fe
Praveen Kumar187 <prave187@in.ibm.com>
2024-05-20 17:06:38 +0530

    update

--- Functions/TL_Dashboard.py
+++ Functions/TL_Dashboard.py
@@ -5,13 +5,13 @@ from hamcrest import *
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
-from pynput.keyboard import Controller, Key
+#from pynput.keyboard import Controller, Key
 import openpyxl
 from utilities.customlogger import LogGen
 from pageObjects.Graphs import graphs
 import allure
 from allure_commons.types import AttachmentType
-from Functions.ScreenshotFile import screenshotfile
+#from Functions.ScreenshotFile import screenshotfile
 
 path = "./testData/Login_shrri.xlsx"
 workbook = openpyxl.load_workbook(path)
